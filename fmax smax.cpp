f7f6b9ee99a970dcf02af236afb6cf66d99daaf7
jnithyashree1-alt <jnithyashree1@gmail.com>
2026-07-16 15:02:34 +0530

    Add files via upload

---
+++ fmax smax.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	int n,fmax,smax=1,curr;
+	scanf("%d%d",&n,&fmax);
+	for(int ctr=2;ctr<=n;ctr++){
+		scanf("%d",&curr);
+		if(curr>fmax){
+			smax=fmax;
+			fmax=curr;
+		}
+		else if(curr>smax){
+			smax=curr;
+		}
+	}
+	printf("%d %d",fmax,smax);
+}
